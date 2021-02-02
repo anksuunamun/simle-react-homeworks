6a5f1d9a1932d990b378463b05d55cb13826898d
anksuunamun <shishkina_veronika_10@mail.ru>
2021-02-02 14:00:09 +0300

    fix: edit Error404.tsx with <div />

--- src/p2-homeworks/h5/pages/Error404.tsx
+++ src/p2-homeworks/h5/pages/Error404.tsx
@@ -4,22 +4,18 @@ import s from './Error404.module.css';
 
 function Error404() {
     return (
-        // <div>
-        //     <div>404</div>
-        //     <div>Page not found!</div>
-        //     <div>—ฅ/ᐠ.̫ .ᐟ\ฅ—</div>
-        // </div>
+
 
         <div className={s.div}>
             <div className={s.face}>
                 <div className={s.band}>
-                    <div className={s.red}></div>
-                    <div className={s.white}></div>
-                    <div className={s.blue}></div>
+                    <div className={s.red}/>
+                    <div className={s.white}/>
+                    <div className={s.blue}/>
                 </div>
-                <div className={s.eyes}></div>
-                <div className={s.dimples}></div>
-                <div className={s.mouth}></div>
+                <div className={s.eyes}/>
+                <div className={s.dimples}/>
+                <div className={s.mouth}/>
             </div>
 
             <h1>404</h1>
