d8e56c228339b78dee90738e87059d7f3f8415c4
anksuunamun <shishkina_veronika_10@mail.ru>
2021-02-02 10:27:25 +0300

    Updates

---
+++ precache-manifest.20fd5dd27ef7bb76580f8ca4fb3835dd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccf16639250d02a95b3ea4d6e2d3be92",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "88263695bb3d8d60b6f8",
+    "url": "/simple-react-homeworks/static/css/main.b287568a.chunk.css"
+  },
+  {
+    "revision": "582af1c1c111b381b748",
+    "url": "/simple-react-homeworks/static/js/2.84b05708.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks/static/js/2.84b05708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88263695bb3d8d60b6f8",
+    "url": "/simple-react-homeworks/static/js/main.ea8540ca.chunk.js"
+  },
+  {
+    "revision": "af4ec8013c5fe87b14da",
+    "url": "/simple-react-homeworks/static/js/runtime-main.f59815ab.js"
+  },
+  {
+    "revision": "207f58739f31af891eaa45245b7b4788",
+    "url": "/simple-react-homeworks/static/media/checkmark.207f5873.svg"
+  },
+  {
+    "revision": "2ff767d159384f9e75ce0311894237a8",
+    "url": "/simple-react-homeworks/static/media/menu.2ff767d1.svg"
+  }
+]);
