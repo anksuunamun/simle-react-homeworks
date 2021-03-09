49eaac5b0cafa624e9485cd805d12ab5c2f08b73
anksuunamun <shishkina_veronika_10@mail.ru>
2021-03-09 12:13:41 +0300

    feat: add HW9

--- src/p2-homeworks/h9/Clock.tsx
+++ src/p2-homeworks/h9/Clock.tsx
@@ -1,47 +1,60 @@
-import React, {useState} from "react";
-import SuperButton from "../h4/common/c2-SuperButton/SuperButton";
+import React, {useState} from 'react';
+import SuperButton from '../h4/common/c2-SuperButton/SuperButton';
+import styles from './Clock.module.css';
 
 function Clock() {
     const [timerId, setTimerId] = useState<number>(0);
-    const [date, setDate] = useState<Date>();
+    const [date, setDate] = useState<Date>(new Date);
     const [show, setShow] = useState<boolean>(false);
 
     const stop = () => {
         // stop
+        clearTimeout(timerId);
     }
     const start = () => {
         stop();
+        //Вызов setTimeout возвращает «идентификатор таймера» timerId, который можно использовать для отмены дальнейшего выполнения.
         const id: number = window.setInterval(() => {
             // setDate
+            setDate(new Date);
         }, 1000);
         setTimerId(id);
     }
 
     const onMouseEnter = () => {
         // show
+        setShow(true);
     };
     const onMouseLeave = () => {
         // close
+        setShow(false);
     };
 
-    const stringTime = "Time"; // fix with date
-    const stringDate = "Date"; // fix with date
+    const addZero = (time: number) => {
+        return time < 10 ? `0${time}` : time;
+    }
+    const hours = date && addZero(date.getHours());
+    const minutes = date && addZero(date.getMinutes());
+    const seconds = date && addZero(date.getSeconds());
+
+    const stringTime = `${hours}:${minutes}:${seconds}`; // fix with date
+
+    const stringDate = `${Intl.DateTimeFormat('en-US', {month: 'long'}).format(date)} ${date.getDate()}, ${Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date)}`; // fix with date
 
     return (
         <div>
-            <div
-                onMouseEnter={onMouseEnter}
-                onMouseLeave={onMouseLeave}
-            >
-                {stringTime}
-            </div>
-
-            {show && (
-                <div>
-                    {stringDate}
+            <div className={styles.clockWrapper}>
+                <div
+                    onMouseEnter={onMouseEnter}
+                    onMouseLeave={onMouseLeave}
+                > {stringTime}
                 </div>
-            )}
-
+                {show && (
+                    <div className={styles.dateWrapper}>
+                        {stringDate}
+                    </div>
+                )}
+            </div>
             <SuperButton onClick={start}>start</SuperButton>
             <SuperButton onClick={stop}>stop</SuperButton>
 
